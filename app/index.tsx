239f10e6ca8d57964d3e9590e1efef61acd59720
Jheff4 <75406244+Jheff4@users.noreply.github.com>
2024-09-19 15:34:20 +0100

    completed auth functionality and auth screens

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,13 @@
 /* eslint-disable prettier/prettier */
 import { Redirect } from "expo-router"
-// import { SafeAreaView } from "react-native-safe-area-context"
+import { useAuth } from "@clerk/clerk-expo"
 
 const Home = () => {
+  const { isSignedIn } = useAuth()
+
+  if (isSignedIn) {
+    return <Redirect href={"/(root)/(tabs)/home"} />
+  }
   return <Redirect href="/(auth)/welcome" />
 }
 
